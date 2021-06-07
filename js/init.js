77029a4f0f960cb87ae0b1c904910c6e9ca77787
propuweb8 <propuweb8@gmail.com>
2021-06-06 19:42:28 -0500

    Cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCapbjMgK9468ICMWWnyrVvTbteXFOx_8E",
+    authDomain: "propweb39.firebaseapp.com",
+    projectId: "propweb39",
+    storageBucket: "propweb39.appspot.com",
+    messagingSenderId: "86992270502",
+    appId: "1:86992270502:web:7c027b65446d5b5102b067",
+    measurementId: "G-RFGPZ8Y89S"
+});
