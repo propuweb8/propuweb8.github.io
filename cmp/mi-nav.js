82a92fff440fe6b4d59a7bc41998bf1efd8d85bc
propuweb8 <85463511+propuweb8@users.noreply.github.com>
2021-06-17 08:47:33 -0500

    Cambio nav

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -14,7 +14,7 @@ class MiNav extends HTMLElement {
       `<ul>
         <li>
           <a href="index.html">
-            Bievenid@</a>
+            Bienvenid@</a>
         </li>
       </ul>`;
     this.ul =
